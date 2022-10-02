6d6b798d8a8c893c0a041a5d3dc64f97b08272b0
Alisher Makhmudov <makhmudov.alisher@gmail.com>
2022-10-02 15:07:06 +0500

    feat: course-single-quiz page

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,26 +1,28 @@
-const slideSwiper = new Swiper(".testimonials-swiper", {
-  speed: 900,
-  grabCursor: true,
-  // autoHeight: true,
-  mousewheel: true,
-  keyboard: true,
-  slidesPerView: 1,
-  spaceBetween: 24,
-  // centeredSlides: true,
-
-  pagination: {
-    el: ".testimonials-swiper-pagination",
-    clickable: true,
-  },
-
-  breakpoints: {
-    // when window width is >= 640px
+  const cinemaThumbs = new Swiper(".cinema-thumbs", {
+    spaceBetween: 12,
+    slidesPerView: 3,
+    watchSlidesProgress: true,
+    allowTouchMove: true,
+    breakpoints: {
+//     // when window width is >= 640px
     640: {
-      slidesPerView: 2,
+      slidesPerView: 6,
     },
-    // when window width is >= 1024px
-    1024: {
-      slidesPerView: 3,
-    },
+  }
+  });
+
+  const cinema = new Swiper(".cinema", {
+    spaceBetween: 12,
+    keyboard: {
+    enabled: true,
+    allowTouchMove: true,
+    onlyInViewport: false,
   },
+    navigation: {
+      nextEl: ".cinema-thumbs-next",
+      prevEl: ".cinema-thumbs-prev",
+    },
+    thumbs: {
+      swiper: cinemaThumbs,
+    },
 });
